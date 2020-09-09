72924c2cd264dfd4f8cf5a0e77e749d991da37bb
erdemcaygor <erdem.caygor@gmail.com>
2020-09-09 23:31:49 +0300

    styles updated

---
+++ src/components/card/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card = (props) => (
+  <div class="card">
+    <div class="card-body">This is some text within a card body.</div>
+  </div>
+);
+
+export { Card };
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,9 +1,49 @@
 import React from "react";
 
+import { Card } from "../../components";
 import "./_home.scss";
 
 const Home = () => {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container container-fluid px-0">
+      <div className="row home-container__sub-container">
+        <div className="col-8">
+          <div className="px-4">
+            <strong>EVENTS</strong>
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+          <div className="pt-1">
+            <Card />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="px-4">
+            <strong>EVENT DETAILS</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
